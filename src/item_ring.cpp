ebb5949915bed6f9a500609a1172bb1ab642b18a
Lee E <98901977+KupferLee@users.noreply.github.com>
2022-09-25 12:41:04 +0200

    ring gives +10 strength instead of +5

--- src/item_ring.cpp
+++ src/item_ring.cpp
@@ -3,14 +3,14 @@
 item_ring::item_ring()
 {
     this->name = "Ring";
-    this->description = "+5 Strength";
+    this->description = "+10 Strength";
     this->value = 7;
     this->weight = 1;
     this->texture_id = 9;
     this->texture = LoadTexture("assets/graphics/gui/item_ring.png");
 
-    // if worn gives a Player +5 strength
-    this->plus_strength = 5;
+    // if worn gives a Player + strength
+    this->plus_strength = 10;
 }
 
 int item_ring::Get_Strength() { return this-> plus_strength; }
